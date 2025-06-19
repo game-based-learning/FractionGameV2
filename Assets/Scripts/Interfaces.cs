7cf70db50b01a5fc514ade1b10d8afd7581b3434
eric-zhang1 <ericletianzhang@gmail.com>
2025-06-19 16:30:00 -0400

    Added IIngredient interfaces

---
+++ Assets/Scripts/Interfaces.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Interfaces
+{
+    /**
+     * Interface for ingredients
+     */
+    public interface IIngredient
+    {
+        // TODO: Once Fraction class is created, uncomment following line
+        // Fraction Value { get; }
+        String Name { get; }
+    }
+}
+
