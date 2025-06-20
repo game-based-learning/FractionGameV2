42adcea7f9bc374aa866aab7d7fd7a2114a67d5a
kani-sha <128569032+kani-sha@users.noreply.github.com>
2025-06-20 17:25:39 -0400

    fraction numerator & denominator & petal class

---
+++ Assets/Scripts/Fraction.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Fraction
+{
+    private int numerator;
+    private int denominator;
+
+    public int Numerator
+    {
+        get { return numerator; }
+        set { numerator = value; }
+    }
+
+    public int Denominator
+    {
+        get { return denominator; }
+        set
+        {
+            if (value == 0)
+            {
+                throw new System.DivideByZeroException("Denominator cannot be zero.");
+            }
+            denominator = value;
+        }
+    }
+
+    // TODO: Make get value a to string
+}
