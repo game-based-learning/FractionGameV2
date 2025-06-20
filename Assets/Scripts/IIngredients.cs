402f773bdeae2b8c49b7edd39048d7a55fa733a6
eric-zhang1 <ericletianzhang@gmail.com>
2025-06-20 18:14:08 -0400

    Merge plant into feat/interfaces

---
+++ Assets/Scripts/IIngredients.cs
@@ -0,0 +1,16 @@
+﻿using Ingredients;
+
+namespace Ingredients
+{
+    public enum IngredientType = {PETAL, POTION, CATALYST}
+    /**
+     * Interface for ingredients
+     */
+    public interface IIngredient
+    {
+        Fraction Value { get; }
+        IngredientType Type { get; }
+        String Name { get; }
+    }
+}
+
