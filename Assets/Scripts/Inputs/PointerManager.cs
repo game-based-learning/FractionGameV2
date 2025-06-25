96c410468aa5dd0e7a2703d49d35892d7ef1345e
eric-zhang1 <ericletianzhang@gmail.com>
2025-06-25 15:49:05 -0400

    Draggable Items are now draggable

---
+++ Assets/Scripts/Inputs/PointerManager.cs
@@ -0,0 +1,64 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utility;
+
+namespace Inputs
+{
+    public class PointerManager : MonoBehaviour
+    {
+        private Camera mainCamera;
+        private InputSystem_Actions inputActions;
+        private InputAction click;
+
+        private GameObject attached = null;
+
+        void Awake()
+        {
+            mainCamera = Camera.main;
+            inputActions = new InputSystem_Actions();
+            click = inputActions.UI.Click;
+
+            // Subscribe the hold and release of the clicks
+            click.started += mouseDown;
+            click.canceled += mouseUp;
+
+            click.Enable();
+        }
+
+        private void FixedUpdate()
+        {
+            transform.position = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        private void mouseDown(InputAction.CallbackContext context)
+        {
+            Vector3 mousePos = Input.mousePosition;
+            Ray ray = mainCamera.ScreenPointToRay(mousePos);
+            // Creates a temporary list of hits
+            List<RaycastHit2D> hits = new List<RaycastHit2D>();
+            if (Physics2D.GetRayIntersection(ray, 50, hits, -5) > 0)
+            {
+                GameObject gameObject = hits[0].transform.gameObject;
+                Draggable item = gameObject.GetComponent<Draggable>();
+                if (item != null)
+                {
+                    // Attach the petal to the pointer object.
+                    attached = gameObject;
+                    attached.transform.SetParent(transform);
+                }
+            }
+        }
+
+        private void mouseUp(InputAction.CallbackContext context)
+        {
+            if (attached != null)
+            {
+                attached.transform.SetParent(null);
+                attached = null;
+            }
+        }
+    }
+}
+
+
--- Assets/Scripts/Utility/Draggable.cs
+++ Assets/Scripts/Utility/Draggable.cs
@@ -1,10 +1,41 @@
+using System;
 using UnityEngine;
 
 namespace Utility
 {
     public abstract class Draggable : MonoBehaviour
     {
-    
+        private bool isHeld = false;
+        public bool IsHeld { get { return isHeld; } }
+
+        private void Start()
+        {
+            isHeld = false;
+        }
+
+        /**
+         * Attaches current game object to pointer
+         * Requires object to not be held
+         */
+        public virtual void Attach()
+        {
+            // If already held, ignore
+            if (isHeld) return;
+
+            isHeld = true;
+        }
+
+        /**
+         * Dettaches current game object from pointer
+         * Requires object to be held
+         */
+        public virtual void Detach()
+        {
+            // If not held, ignore
+            if (!isHeld) return;
+
+            isHeld = false;
+        }
     }
 }
 
