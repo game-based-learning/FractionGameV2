8e2618087c079f8f1d23e29bdcad09d56f24d123
eric-zhang1 <ericletianzhang@gmail.com>
2025-07-09 14:38:59 -0400

    Created Recipe Manager singleton file

---
+++ Assets/Scripts/Utility/RecipeManager.cs
@@ -0,0 +1,17 @@
+namespace FractionGame.Utility
+{
+    public class RecipeManager
+    {
+        private static RecipeManager instance;
+
+        private RecipeManager() { }
+
+        public static RecipeManager GetInstance()
+        {
+            if (instance == null)
+            {
+                instance = new RecipeManager();
+            }
+            return instance;
+        }
+}
