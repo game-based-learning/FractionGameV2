49ae4f4df64b6954bbda8bd120cdfdf68f485d5c
eric-zhang1 <ericletianzhang@gmail.com>
2025-07-14 14:42:09 -0400

    Added Recipe and readied script for adding recipies.

--- Assets/Scripts/Utility/RecipeManager.cs
+++ Assets/Scripts/Utility/RecipeManager.cs
@@ -1,10 +1,34 @@
+using FractionGame.Ingredients;
+using System.Collections.Generic;
+
 namespace FractionGame.Utility
 {
     public class RecipeManager
     {
         private static RecipeManager instance;
+        private List<Recipe> recipeList;
 
-        private RecipeManager() { }
+        private class Recipe
+        {
+            private Dictionary<PlantType, int> ingredients;
+
+
+            public Recipe(string name, params (PlantType, int)[] pairs)
+            {
+                // Add each plant type, number 
+                foreach ((PlantType, int) pair in pairs)
+                {
+                    ingredients.Add(pair.Item1, pair.Item2);
+                }
+            }
+        }
+
+        private RecipeManager()
+        {
+            recipeList = new List<Recipe>()
+            {
+            };
+        }
 
         public static RecipeManager GetInstance()
         {
